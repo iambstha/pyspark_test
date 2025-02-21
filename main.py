3e9f1e781f5f3bba049712094b70ab012cf34dab
Nishan Khatiwada <nishantkhatiwada012@gmail.com>
2025-02-21 16:34:58 +0545

    benchmarking added

--- main.py
+++ main.py
@@ -7,31 +7,37 @@ from helper.schema_def import schema
 from helper.parser import parse_fixed_width
 from scripts.start_read_and_write import start_read_and_write
 
+from util.benchmark import Benchmark
+
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 if __name__ == "__main__":
 
+    benchmark = Benchmark()
+
     cpu_cores = multiprocessing.cpu_count()
+    file_path = "data/large_fixed_width_data_1gb.txt"
 
-    print("Starting Spark session...")
-    spark = SparkSession.builder \
-        .appName("Optimized Read and Write") \
-        .config("spark.jars", "lib/postgresql-42.7.5.jar") \
-        .config("spark.sql.shuffle.partitions", str(cpu_cores * 2)) \
-        .config("spark.executor.extraJavaOptions", "-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35") \
-        .config("spark.sql.autoBroadcastJoinThreshold", -1) \
-        .config("spark.memory.offHeap.enabled","true") \
-        .config("spark.memory.offHeap.size","10g") \
-        .master("local[*]") \
-        .getOrCreate()
+    with benchmark.measure("Start Spark Session"):
+        spark = SparkSession.builder \
+            .appName("Optimized Read and Write") \
+            .config("spark.jars", "lib/postgresql-42.7.5.jar") \
+            .config("spark.sql.shuffle.partitions", str(cpu_cores * 2)) \
+            .config("spark.executor.extraJavaOptions", "-XX:+UseG1GC -XX:InitiatingHeapOccupancyPercent=35") \
+            .config("spark.sql.autoBroadcastJoinThreshold", -1) \
+            .config("spark.memory.offHeap.enabled","true") \
+            .config("spark.memory.offHeap.size","10g") \
+            .master("local[*]") \
+            .getOrCreate()
 
-    print("Spark session started")
+    print("Spark session started . . .")
 
-    try:
-        start_read_and_write(spark)
-    except Exception as e:
-        print(f"❌ Error occurred: {e}")
+    with benchmark.measure("Read and Write"):
+        try:
+            start_read_and_write(spark, file_path)
+        except Exception as e:
+            print(f"❌ Error occurred: {e}")
 
     spark.stop()
     print("Spark session stopped")
